4c5d20125568299cac31bfa5d4f9d2f666c263ba
Mahamuni <pallavi.mahamuni@capgemini.com>
2017-12-27 14:37:02 +0530

    Added filter

--- app/product/product.component.ts
+++ app/product/product.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { IProduct } from "./product";
 
 @Component({
   selector: 'pm-products',
   templateUrl: './app/product/product-list.component.html'
 })
 export class ProductComponent{
+    pageTitle: string ="Product List";
+    filteredProduct: IProduct[];
+    _listFilter: string = '';
     showImage: boolean;
     imageWidth: number = 50;
     imageMargin: number = 2;
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
@@ -30,8 +34,27 @@ export class ProductComponent{
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
         }
     ];
+    constructor(){
+         console.log("constructor called");
+        this.filteredProduct=this.products;
+    }
     toggleImage():void{
      this.showImage =! this.showImage;
         
     }
+    //getter and setter of listFilter for 2 way binding
+    get listFilter(): string{
+        return this._listFilter;
+    }
+    set listFilter(value:string){
+         this._listFilter=value;
+         this.filteredProduct=this.performFilter(this._listFilter);
+         console.log(value);
+    }
+    performFilter(filterBy: string): IProduct[] {
+        console.log("filterBy "+filterBy);
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+              product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }	
 }
